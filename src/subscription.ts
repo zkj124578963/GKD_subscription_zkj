b021f4a1d7dee6f540971734635522900b13ac4e
张开金 <77008103+zkj124578963@users.noreply.github.com>
2026-06-04 22:09:44 +0800

    提交-04

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,11 +4,16 @@ import categories from './categories';
 import globalGroups, { OPEN_AD_ORDER } from './globalGroups';
 import { RawApp, RawAppGroup } from '@gkd-kit/api';
 
-const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+// 只读取apps目录，过滤所有package.json
+const allApps = await batchImportApps(`${import.meta.dirname}/apps`);
+// 过滤排除package.json配置文件
+const apps = allApps.filter(item => !item.file?.includes('package.json'));
+
 const rawApps: RawApp[] = [];
 apps.forEach((appConfig) => {
   appConfig.groups?.forEach((g: RawAppGroup) => {
-    if (g.name.startsWith('')) {
+    // 空字符串无效，改成匹配开屏广告
+    if (g.name.startsWith('开屏广告')) {
       g.order = OPEN_AD_ORDER;
     }
   });
@@ -21,6 +26,7 @@ export default defineGkdSubscription({
   version: 0,
   author: 'AIsouler',
   checkUpdateUrl: './AIsouler_gkd.version.json5',
+  // 如需国内可访问可替换链接，保留原链接不影响校验报错
   supportUri: 'https://github.com/AIsouler/GKD_subscription/issues/new/choose',
   categories,
   globalGroups,
